0f8c0d3b275878863fcea219a2793b58d4f9e615
Fahad87667 <ftkhan61814@gmail.com>
2024-02-16 12:56:41 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyphen_e = '-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\nf","") for req in requirements]
+        
+        if hyphen_e in requirements:
+            requirements.remove(hyphen_e)
+            
+    return requirements
+
+setup(
+    name='Mumbai House Price Prediction',
+    version='0.0.1',
+    author='Fahad',
+    author_email='ftkhan61814@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+    )
