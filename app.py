59f0a6d210a104c6392b504b9da8f6255c5daa89
Fahad87667 <ftkhan61814@gmail.com>
2024-02-16 16:24:19 +0530

    Prediction

--- app.py
+++ app.py
@@ -3,12 +3,35 @@ import pandas as pd
 import pickle
 
 app = Flask(__name__)
-
+data = pd.read_csv('./cleaned_data.csv')
+pipe = pickle.load(open("model.pkl","rb"))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    regions = sorted(data['region'].unique())
+    types = sorted(data['type'].unique())
+    statuss = sorted(data['status'].unique())
+    
+    return render_template('index.html', regions=regions, types=types, statuss=statuss)
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    bhk = request.form.get('bhk')
+    type= request.form.get('type')
+    area= request.form.get('area')
+    region= request.form.get('region')
+    status= request.form.get('status')
+    price = request.form.get('price')
+    
+    
+    print(bhk,type,area,region,status,price)
+    input = pd.DataFrame([[bhk,type,area,region,status,price]],columns=['bhk','type','area','region','status','price'])
+    prediction=pipe.predict(input)[0]
+    
+     # Format prediction as a string with currency symbol
+    formatted_prediction = f'₹ {prediction:.2f} Lacs'
+    
+    return render_template('result.html', prediction=formatted_prediction)
 
 if __name__=='__main__':
     app.run(debug=True)
